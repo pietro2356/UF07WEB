a320e5b3a4186624704a5c7b88864122094fdeae
StefanoPlotegher <stefano.plotegher@marconirovereto.it>
2025-02-07 08:13:39 +0100

    Secondo esecizio JS: Javascript
    implementate due funzioni per invertire il valore di due variabili

--- src/app.js
+++ src/app.js
@@ -9,3 +9,21 @@ function stampa1(nome,cognome,matricola){
 function stampa2(nome,cognome,matricola){
     console.log("Sono " + nome, cognome + " e la mia matricola è: " + matricola);
 }
+
+//inversione di 2 variabili con variabile temporanea
+function inverti1(nome,cognome){
+    let tmp = nome;
+    nomeInv = cognome;
+    cognomeInv = tmp;
+
+    console.log("Variabile nome inverita: " + nomeInv);
+    console.log("Variabile cognome invertita: " + cognomeInv);
+}
+
+//inversione di 2 variabili senza variabile temporanea
+function inverti2(nome, cognome) {
+    [nome, cognome] = [cognome, nome];
+
+    console.log("Variabile nome inverita: " + nome);
+    console.log("Variabile cognome invertita: " + cognome);
+}
