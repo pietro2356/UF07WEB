af88552a6027d53488edf170939db89bc4c54782
StefanoPlotegher <stefano.plotegher@marconirovereto.it>
2025-02-07 08:12:04 +0100

    Primo Esercizzio JS: completato
    Inseriti due pulsanti e due funzioni per stampare tre variabili in sequenza o concatenate

---
+++ src/app.js
@@ -0,0 +1,11 @@
+//Stampa in sequenza di 3 variabili
+function stampa1(nome,cognome,matricola){
+    console.log(nome);
+    console.log(cognome);
+    console.log(matricola);
+}
+
+//stampare conatenata di 3 variabili
+function stampa2(nome,cognome,matricola){
+    console.log("Sono " + nome, cognome + " e la mia matricola è: " + matricola);
+}
