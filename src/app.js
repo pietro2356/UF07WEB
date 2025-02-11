b5e00b2c45e62e4d1c65eb65845117a7a52e808b
Matteo <matteo.bertotti@mat.tn.it>
2025-02-11 20:14:29 +0100

    Esercizio 2 JS

--- src/app.js
+++ src/app.js
@@ -2,11 +2,31 @@
  * ESERCIZIO 1
  */
 function esercizio1(){
-    const nome = 'Matteo';
-    const cognome = 'Bertotti';
-    const matricola = '123456';
+    let nome = 'Matteo';
+    let cognome = 'Bertotti';
+    let matricola = '123456';
     console.log(nome);
     console.log(cognome);
     console.log(matricola);
     console.log(`Sono ${nome} ${cognome} e la mia matricola è ${matricola}`);
+}
+/**
+ * ESERCIZIO 2
+ */
+function esercizio2a(){
+    let nome = 'Matteo';
+    let cognome = 'Bertotti';
+    let temp = nome;
+    nome = cognome;
+    cognome = temp;
+    console.log(nome); // Bertotti
+    console.log(cognome); // Matteo
+}
+
+function esercizio2b(){
+    let nome = 'Matteo';
+    let cognome = 'Bertotti';
+    [nome, cognome] = [cognome, nome];
+    console.log(nome); // Bertotti
+    console.log(cognome); // Matteo
 }
