656db49890339bc7363d6fda0b2a2a262ce80da0
Matteo <matteo.bertotti@mat.tn.it>
2025-02-11 20:11:46 +0100

    Esercizio 1 JS

---
+++ src/app.js
@@ -0,0 +1,12 @@
+/**
+ * ESERCIZIO 1
+ */
+function esercizio1(){
+    const nome = 'Matteo';
+    const cognome = 'Bertotti';
+    const matricola = '123456';
+    console.log(nome);
+    console.log(cognome);
+    console.log(matricola);
+    console.log(`Sono ${nome} ${cognome} e la mia matricola è ${matricola}`);
+}
