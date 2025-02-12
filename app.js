e520a46b17d93eb5eeecdf63469ab519d28bfae4
Daniel_Mittem <daniel.mittempergher@marconirovereto.it>
2025-02-12 17:29:22 +0100

    Creato ciclo for e ciclo while con implementazione di bottone per i vari cicli

--- app.js
+++ app.js
@@ -2,6 +2,7 @@ let nome = "Mittemperher ";
 let cognome = "Daniel ";
 let matricola = "238734 ";
 let punteggio = 0;
+const sufficienza = 18;
 
 //Esercizio 2
 [nome, cognome] = [cognome, nome];
@@ -17,20 +18,59 @@ console.log(typeof matricolaNumero);
 //Esercizio 4
 console.log("Punteggio di partenza = " + punteggio);
 punteggio += 13;
-console.log("Punteggio = " + punteggio);
+console.log("Punteggio addizione = " + punteggio);
 punteggio -= 4;
 console.log("Punteggio sottrazione = " + punteggio);
-punteggio *= 3;
+punteggio *= 4;
 console.log("Punteggio moltiplicato = " + punteggio);
 punteggio /= 2;
 console.log("Punteggio diviso = " + punteggio);
 punteggio **= 2;
 console.log("Punteggio alla terza = " + punteggio);
 
-//esercizio 5 
+//Esercizio 5 
 if (punteggio > 314) {
     console.log("Ottimo punteggio");
 } else {
     console.log("Buon punteggio");
 }
 
+//Esercizio 6
+//ciclo for
+function Esercizio6For(){
+    console.log("Ciclo for:");
+        for (let i = 0; i <= 32; i++) {
+            if (i > sufficienza) {
+                if (i >= 31) {
+                    console.log("30L");
+                } else {
+                    console.log(i);
+                }
+            }
+        }
+    }
+
+//ciclo while
+function Esercizio6While(){
+    console.log("Ciclo while:");
+    let i = sufficienza + 1;
+    while (i <= 32) {
+        console.log(i);
+        i++;
+    }
+}
+
+//Esercizio 6 pt.2
+//ciclo for
+function Esercizio6For2(){
+    console.log("Ciclo for:");
+        for (let i = 0; i <= 32; i++) {
+            if (i > sufficienza) {
+                if (i >= 31) {
+                    console.log("30L");
+                } else {
+                    console.log(i);
+                }
+            }
+        }
+    }
