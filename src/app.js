d970eec599bcd382c77d35aafe13e0d177f4357d
manuel.mezzanotte <manuel.mezzanotte@mat.tn.it>
2025-02-12 16:59:29 +0100

    Creazione esercizio 1 JS collegamento script.

---
+++ src/app.js
@@ -0,0 +1,10 @@
+let nome = "Dio";
+let cognome = "Brando";
+let matricola = "123456";
+
+function esercizio1(){
+    console.log("nome");
+    console.log("cognome");
+    console.log("matricola");
+    console.log(`Nome: ${nome} Cognome: ${cognome} Matricola: ${matricola}`);
+}
