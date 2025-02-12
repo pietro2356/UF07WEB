ff90c344402439e6a2a6f242c59a8fc7280dfd44
Daniel_Mittem <daniel.mittempergher@marconirovereto.it>
2025-02-12 15:30:44 +0100

    scambio di valori senza usare una terza variabile

--- app.js
+++ app.js
@@ -1,8 +1,7 @@
 let nome = "Mittemperher ";
 let cognome = "Daniel ";
 let matricola = "829DA ";
-let scambio = nome;
-nome = cognome;
-cognome = scambio;
+[nome, cognome] = [cognome, nome];
+
 
 console.log("Sono " + nome + cognome + "e la mia matricola: " + matricola);
