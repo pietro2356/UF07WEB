730bc301ddd150b8ae7d9f7f2bd68a0023107829
Daniel_Mittem <daniel.mittempergher@marconirovereto.it>
2025-02-12 15:09:45 +0100

    Aggiunto collegamento JS, creato le prime variabili e stampate nel terminale

---
+++ app.js
@@ -0,0 +1,9 @@
+let nome = "Daniel ";
+let cognome = "Mittempergher ";
+let matricola = "829DA ";
+
+console.log (nome);
+console.log (cognome);
+console.log (matricola);
+
+console.log("Sono " + nome + cognome + "e la mia matricola: " + matricola);
