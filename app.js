d55979a7ce4887f7d9f9291770178ccfb9cfcd0a
Daniel_Mittem <daniel.mittempergher@marconirovereto.it>
2025-02-12 15:52:47 +0100

    Stampato numeri in console (sottrazioni, addizioni, divisioni, moltiplicazioni, elevazioni)

--- app.js
+++ app.js
@@ -1,13 +1,30 @@
 let nome = "Mittemperher ";
 let cognome = "Daniel ";
 let matricola = "238734 ";
-[nome, cognome] = [cognome, nome];
+let punteggio = 0;
 
+//Esercizio 2
+[nome, cognome] = [cognome, nome];
+console.log("Sono " + nome + cognome + "e la mia matricola: " + matricola);
+
+//Esercizio 3
 let matricolaStringa = String(matricola);  
 console.log(typeof matricolaStringa);  
 
 let matricolaNumero = Number(matricolaStringa); 
 console.log(typeof matricolaNumero); 
 
+//Esercizio 4
+console.log("Punteggio di partenza = " + punteggio);
+punteggio += 13;
+console.log("Punteggio = " + punteggio);
+punteggio -= 4;
+console.log("Punteggio sottrazione = " + punteggio);
+punteggio *= 3;
+console.log("Punteggio moltiplicato = " + punteggio);
+punteggio /= 2;
+console.log("Punteggio diviso = " + punteggio);
+punteggio **= 2;
+console.log("Punteggio alla terza = " + punteggio);
 
-console.log("Sono " + nome + cognome + "e la mia matricola: " + matricola);
+
