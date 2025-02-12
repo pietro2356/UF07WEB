173ce4b54a3b106b264cd140adbc020d3bd67ad4
Daniel_Mittem <daniel.mittempergher@marconirovereto.it>
2025-02-12 15:40:51 +0100

    Convertito un numero in stringa e una stringa in numero

--- app.js
+++ app.js
@@ -1,7 +1,13 @@
 let nome = "Mittemperher ";
 let cognome = "Daniel ";
-let matricola = "829DA ";
+let matricola = "238734 ";
 [nome, cognome] = [cognome, nome];
+
+let matricolaStringa = String(matricola);  
+console.log(typeof matricolaStringa);  
+
+let matricolaNumero = Number(matricolaStringa); 
+console.log(typeof matricolaNumero); 
 
 
 console.log("Sono " + nome + cognome + "e la mia matricola: " + matricola);
