ab21fab3397e3b09a838cab510f94cbc9c41d859
manuel.mezzanotte <manuel.mezzanotte@mat.tn.it>
2025-02-12 18:04:24 +0100

    creazione esercizio 8

--- src/app.js
+++ src/app.js
@@ -54,7 +54,7 @@ function esercizio6for(){
     
 }
 function esercizio6while(){
-    console.log("con while");
+    console.log(`con while`);
     let i = 0;
     while (i <= 32) {
         let voto = i;
@@ -71,11 +71,11 @@ const VOTI = [0, 1, 2, 3, 4, 5, 6 ,7 ,8 ,9 ,10, 11, 12, 13, 14, 15, 16, 17, 18, 
 function esercizio6voto(){
     for (let i = 0; i < VOTI.length; i++) {
         switch (true) {
-            case (VOTI[i] <= 16): console.log("Insufficiente fai cagare Dio"); break;
-            case (VOTI[i] <= 20): console.log("discreto passato per dio"); break;
-            case (VOTI[i] <= 24): console.log("buono mah potevi fare di più studia coglion"); break;
-            case (VOTI[i] <= 28): console.log("distinto bravo dio ma non ti montare la testa"); break;
-            case (VOTI[i] <= 32 ): console.log("ottimo, sei stato bravissimo a copiare"); break;
+            case (VOTI[i] <= 16): console.log(`Insufficiente fai cagare Dio`); break;
+            case (VOTI[i] <= 20): console.log(`discreto passato per dio`); break;
+            case (VOTI[i] <= 24): console.log(`buono mah potevi fare di più studia coglion`); break;
+            case (VOTI[i] <= 28): console.log(`distinto bravo dio ma non ti montare la testa`); break;
+            case (VOTI[i] <= 32 ): console.log(`ottimo, sei stato bravissimo a copiare`); break;
         }
     }
     
@@ -85,13 +85,25 @@ function esercizio7(){
     voto = document.forms["votoDiDio"]["voto"].value;
     if (voto >= 0 && voto <= 32) {
         switch (true) {
-            case (voto <= 16): console.log("Insufficiente fai cagare Dio"); break;
-            case (voto <= 20): console.log("discreto passato per dio"); break;
-            case (voto <= 24): console.log("buono mah potevi fare di più studia coglion"); break;
-            case (voto <= 28): console.log("distinto bravo dio ma non ti montare la testa"); break;
-            case (voto <= 32 ): console.log("ottimo, sei stato bravissimo a copiare"); break;
+            case (voto <= 16): console.log(`Insufficiente fai cagare Dio`); break;
+            case (voto <= 20): console.log(`discreto passato per dio`); break;
+            case (voto <= 24): console.log(`buono mah potevi fare di più studia coglion`); break;
+            case (voto <= 28): console.log(`distinto bravo dio ma non ti montare la testa`); break;
+            case (voto <= 32 ): console.log(`ottimo, sei stato bravissimo a copiare`); break;
         }
     }else{
-        console.log("See certo e tu credi che " + voto + " sia un voto valido??");
+        console.log(`See certo e tu credi che ` + voto + ` sia un voto valido??`);
+    }
+}
+
+function esercizio8(){
+    let parola = document.forms["rangeVotoDiDio"]["parola"].value.trim().toLowerCase(); 
+    switch (parola) {
+        case "insufficiente": console.log(`Insufficiente: range tra 0-16`); break;
+        case "discreto": console.log(`Discreto: range tra 17-20`); break;
+        case "buono": console.log(`Buono: range tra 21-24`); break;
+        case "distinto": console.log(`Distinto: range tra 25-28`); break;
+        case "ottimo": console.log(`Ottimo: range tra 29-32`); break;
+        default: console.log(`${voto} non è una parola non valida`);
     }
 }
