f62a2d26bc05f9ed8e430db3becc87a6f774888b
Mirko Tevini <mirko.tevini@mat.tn.it>
2025-02-13 10:49:38 +0100

    esercizio2

--- app.js
+++ app.js
@@ -1,5 +1,42 @@
-let tavolo = {};
-let nome = "Mirko";
+let nome = "Pietro";
+let eta = 19;
 
-console.log(tavolo);
-console.log(Mirko);
+//Primo metodo per creare una funzione
+function somma(val1, val2){
+    return val1 + val2;
+}
+
+let result = somma(2, 4);
+console.log("Risultato: ", result);
+
+
+function sottrai(val1, val2){
+    return val1 - val2;
+}
+
+result = sottrai(2, 4);
+console.log("Risultato: ", result);
+
+//Secondo metodo per creare una funzione
+let moltiplica = (val1, val2) => {
+    return val1*val2;
+}
+
+result = moltiplica(2, 4);
+console.log("risultato ", result);
+
+
+
+
+
+
+
+
+
+
+/*true && false
+if(4 == "4" && (typeof 4 == typeof "4")){
+
+}else{
+
+}*/
