e106a26408ec307a62cf2708487a7d16244a9749
emmanuele.grassi <emmanuele.grassi@marconirovereto.it>
2025-02-13 19:02:22 +0100

    feat: scambio nome e cognome

--- src/app.js
+++ src/app.js
@@ -3,9 +3,29 @@ let cognome="Grassi";
 let matricola=239734;
 
 function esercizio1(){
-    console.log(nome)
-    console.log(cognome)
-    console.log(matricola)
-    console.log(nome+" "+cognome+" "+matricola);
+    console.log(nome);
+    console.log(cognome);
+    console.log(matricola);
+    console.log(`Sono ${nome} ${cognome} e la mia matricola è ${matricola}`);
 }
 
+function esercizio2(){
+    console.log("Con 3 variabili:");
+    //scambio nome e cognome con 3 variabili
+    console.log(`Nome:${nome}`);
+    console.log(`Cognome:${cognome}`);
+    let cambio="";
+
+    cambio=cognome; 
+    cognome=nome;
+    nome=cambio;
+
+    console.log(`Nome:${nome}`);
+    console.log(`Cognome:${cognome}`);
+
+    console.log("Con 2 variabili: (ripristino valori)");
+    //scambio nome e cognome con 2 variabili
+    [nome,cognome]=[cognome,nome];
+    console.log(`Nome:${nome}`);
+    console.log(`Cognome:${cognome}`);
+}
