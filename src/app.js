51e79a7edcf1796bd0e9102d289140e389fe1ebf
emmanuele.grassi <emmanuele.grassi@marconirovereto.it>
2025-02-13 18:55:37 +0100

    style: formattato bottoni per esercizi JS

--- src/app.js
+++ src/app.js
@@ -3,5 +3,9 @@ let cognome="Grassi";
 let matricola=239734;
 
 function esercizio1(){
-
+    console.log(nome)
+    console.log(cognome)
+    console.log(matricola)
+    console.log(nome+" "+cognome+" "+matricola);
 }
+
