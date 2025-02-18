964bd70ffea8ebeff00aa4f46df3f8d74f131f7a
Angela Trentin <angela.trentin@oribruni.com>
2025-02-18 19:02:00 +0100

    creata variabile punteggio e varie operazioni

--- esercizi/app.js
+++ esercizi/app.js
@@ -1,6 +1,11 @@
 let nome="Angela";
 let cognome="Trentin";
-const matricola="503530";
+let matricola=238734;
+
+let matricolaStringa=matricola.toString();
+matricolaStringa=Number(matricolaStringa);
 
 [nome,cognome]=[cognome,nome];
 console.log(`Sono ${nome} ${cognome} e la mia matricola è: ${matricola}`);
+console.log(typeof matricola);
+console.log(typeof matricolaStringa);
