964abb28600447ecc4038bbc31f50ed45a7349c7
Angela Trentin <angela.trentin@oribruni.com>
2025-02-18 18:51:17 +0100

    convertito numero in stringa con verifica della conversione

--- esercizi/app.js
+++ esercizi/app.js
@@ -1,5 +1,6 @@
-const nome="Angela";
-const cognome="Trentin";
+let nome="Angela";
+let cognome="Trentin";
 const matricola="503530";
 
-console.log ("Sono "+ nome +" " +cognome + " e la mia matricola è: " +matricola);
+[nome,cognome]=[cognome,nome];
+console.log(`Sono ${nome} ${cognome} e la mia matricola è: ${matricola}`);
